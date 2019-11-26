45312a17a730dd66bae9b192963364609794be5a
erikvw <ew2789@gmail.com>
2019-11-25 22:45:54 -0600

    codenames param on group updater

--- meta_auth/codenames_by_group.py
+++ meta_auth/codenames_by_group.py
@@ -4,7 +4,7 @@ from edc_auth import (
     SCREENING,
     UNBLINDING_REQUESTORS,
     UNBLINDING_REVIEWERS,
-    codenames_by_group,
+    get_default_codenames_by_group,
 )
 
 from .codenames import (
@@ -16,8 +16,11 @@ from .codenames import (
 )
 
 
-codenames_by_group[AUDITOR] = auditor
-codenames_by_group[CLINIC] = clinic
-codenames_by_group[SCREENING] = screening
-codenames_by_group[UNBLINDING_REQUESTORS] = unblinding_requestors
-codenames_by_group[UNBLINDING_REVIEWERS] = unblinding_reviewers
+def get_codenames_by_group():
+    codenames_by_group = {k: v for k, v in get_default_codenames_by_group().items()}
+    codenames_by_group[AUDITOR] = auditor
+    codenames_by_group[CLINIC] = clinic
+    codenames_by_group[SCREENING] = screening
+    codenames_by_group[UNBLINDING_REQUESTORS] = unblinding_requestors
+    codenames_by_group[UNBLINDING_REVIEWERS] = unblinding_reviewers
+    return codenames_by_group
